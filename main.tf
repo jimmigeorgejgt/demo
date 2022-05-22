28e85c436726b709e4cbbd340f5ef15e6c4e9f0e
jimmigeorgejgt <106028854+jimmigeorgejgt@users.noreply.github.com>
2022-05-22 22:36:01 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terrabackec2"
+    key            = "terraform-state"
+    region         = "us-east-1"
+  }
+}
+resource "aws_instance" "app_server" {
+  ami           = "ami-06eecef118bbf9259"
+  instance_type = "t2.micro"
+  tags = {
+    Name = var.instance_name
+  }
+}
